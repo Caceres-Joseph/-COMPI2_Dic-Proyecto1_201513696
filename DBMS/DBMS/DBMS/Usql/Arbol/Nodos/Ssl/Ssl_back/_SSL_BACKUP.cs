845c29f398622ada16ccbe8cc1b769d88b5f99d5
Caceres-Joseph <josephccaceres@gmail.com>
2018-12-12 15:46:02 -0600

    Agregando Nodo bakcup

---
+++ DBMS/DBMS/DBMS/Usql/Arbol/Nodos/Ssl/Ssl_back/_SSL_BACKUP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBMS.Usql.Arbol.Elementos.Tablas;
+
+namespace DBMS.Usql.Arbol.Nodos.Ssl.Ssl_back
+{
+    class _SSL_BACKUP : nodoModelo
+    {
+        public _SSL_BACKUP(string nombre, tablaSimbolos tabla) : base(nombre, tabla)
+        {
+        }
+    }
+}
---
+++ DBMS/DBMS/DBMS/Usql/Arbol/Nodos/Ssl/Ssl_back/_SSL_COMPLETO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBMS.Usql.Arbol.Elementos.Tablas;
+
+namespace DBMS.Usql.Arbol.Nodos.Ssl.Ssl_back
+{
+    class _SSL_COMPLETO : nodoModelo
+    {
+        public _SSL_COMPLETO(string nombre, tablaSimbolos tabla) : base(nombre, tabla)
+        {
+        }
+    }
+}
